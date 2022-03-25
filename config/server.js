5df45931ba52e88b0edd86554c7650f131da6690
Raul_camacho <osvaldoracm@gmail.com>
2022-03-24 18:33:37 -0600

    deploy strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '57812d364684da3d045dc23242d74311'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '82ec8803-f8f4-4dc3-b698-6c12f6e15356'
+};
